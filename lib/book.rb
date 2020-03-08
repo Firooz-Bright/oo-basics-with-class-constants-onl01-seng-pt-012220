84f28acee533ce71d480d8808669a55755f510d3
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-03-08 01:05:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,19 @@
 class Book
-  attr_accessor :author, :page_count 
-  attr_reader :title,:genre
-  @@GENRES=[]
-  
-  def initialize(title,genre)
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
+
+  def initialize(title)
     @title = title
-    @genre = genre
-    @@GENRES << genre
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
- #def genre=(genre)
-  # @genre=genre
-  # GENRES << genre
- #end
-end 
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+end
